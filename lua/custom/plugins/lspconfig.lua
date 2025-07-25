4e8d772c6781007978074248920a227359787d21
Kenny Ek <kennyswek@gmail.com>
2025-07-25 16:52:58 +0200

    fix(lsp): ensure biome is installed

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -126,6 +126,7 @@ return {
     }
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
+      'biome',
       'stylua',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
